94ac05f8878efbdff6606b1ed0a2f8f2d414db27
zorak <slavasitnikov8@gmail.com>
2014-08-18 23:58:15 +0700

    init

---
+++ haxeBigInteger/src/BigInteger.hx
@@ -0,0 +1,7 @@
+package ;
+
+class BigInteger{
+	public function new():Void{
+
+	}
+}
---
+++ haxeBigInteger/test/BigIntegerTest.hx
@@ -0,0 +1,34 @@
+package;
+
+import massive.munit.Assert;
+
+class BigIntegerTest{
+	public function new(){
+
+	}
+	
+	@BeforeClass
+	public function beforeClass():Void{
+
+	}
+	
+	@AfterClass
+	public function afterClass():Void{
+	}
+	
+	@Before
+	public function setup():Void{
+
+	}
+	
+	@After
+	public function tearDown():Void{
+
+	}
+	
+//
+//	@Test
+//	public function emptyStringReturnsZero():Void {
+//
+//	}
+}
