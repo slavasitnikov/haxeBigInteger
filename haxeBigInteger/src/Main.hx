9a02380a4bbd1c8eeb94a0f2c7547ed0dab467aa
zorak <slavasitnikov8@gmail.com>
2014-08-20 00:38:30 +0700

    remove debug code

--- haxeBigInteger/src/Main.hx
+++ haxeBigInteger/src/Main.hx
@@ -1,13 +1,9 @@
 package;
 
-import BigInt;
 import flash.display.Sprite;
 
 class Main extends Sprite {
 	public function new () {
-		super ();
-
-		var i:Int = -1 * (1 << 31);
-		var b = BigInt.fromInt(i);
+		super();
 	}
 }
